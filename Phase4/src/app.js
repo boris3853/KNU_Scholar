f99baa07de338fe4cd2aaa8bfd25b8e017be2025
Kyung <boris3853@naver.com>
2022-12-06 11:24:16 +0000

    added DELETE POST

--- Phase4/src/app.js
+++ Phase4/src/app.js
@@ -804,6 +804,96 @@ app.post('/Article/AddComment', async(req, res) =>{
 });
 
 
+app.get("/Article/Delete", async(req, res) => {
+    kind_ = req.query.kind;
+    
+    if(kind_ === "공지") bind = "ANN";
+    else if(kind_ === "자유글") bind = "GNL";
+    else if(kind_ === "QnA") bind = "QNA";
+    
+    let connection;
+    var context;
+    var results;
+
+    try {
+        // Get a connection from the default pool
+        connection = await oracledb.getConnection('MainP');
+
+        console.log(req.query.pno);
+        // QUERY 1
+        let sql = "DELETE FROM POST WHERE P_NO = :PNO";
+        var binds = [
+            req.query.pno
+        ];
+
+        results = await connection.execute(sql, binds);
+        
+        
+
+        
+        // QUERY 2
+        sql = 'SELECT p_no, title, id, date_time FROM POST WHERE CATEGORY = :KIND';
+
+        binds = [
+            bind
+        ];
+        
+        results = await connection.execute(sql, binds);
+
+        console.dir(results)
+        
+        // var DTM = [];
+        // for(var i=0;i<results.length;++i){
+        //     var YY = new Date(results.rows[i][2]).getFullYear();
+        //     var MM = new Date(results.rows[i][2]).getMonth() + 1;
+        //     var DD = new Date(results.rows[i][2]).getDay();
+        //     // DTM.push("".concat(YY,"-",MM,"-",DD));
+        //     DTM.push("d");
+        // }
+
+        // console.log(DTM);
+
+        context = {results:results.rows};
+        
+
+        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+                
+        
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (connection) {
+            try {
+                // Put the connection back in the pool
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+
+    // 비로그인 상태면 bookmark1_NL.ejs 화면
+    if(!LoginState)
+    {
+        context.id = "Anonymous";
+        context.LS = 0;
+    }
+    // res.render(__dirname + , {post: "공지", id: "Anonymous", LS:0});
+    // 로그인 상태면 bookmark1_L.ejs 화면
+    else 
+    {
+        context.id = MainID;
+        context.LS = 1;
+    }
+
+    context.post = kind_;
+    res.render(__dirname + '/views/post1', context, function(err,html){
+        res.end(html);
+    })
+});
+
+
+
 //////////////////////////////////////////////////////////
 
 
